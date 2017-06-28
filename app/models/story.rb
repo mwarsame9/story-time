666290d1f20434b7c183a90ba42f26f3b13f36f6
Kai de Rochemont and Mohamed Warsame <kai@>
2017-06-28 16:19:33 -0700

    add scope for story with most sentences

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,4 +1,12 @@
 class Story < ActiveRecord::Base
   has_many :sentences
   validates :name, :presence => true
+
+  scope :most_sentences, -> {(
+    select("stories.id, stories.name, stories.image, count(sentences.id) as sentences_count")
+    .joins(:sentences)
+    .group("stories.id")
+    .order("sentences_count DESC")
+    .limit(1)
+    )}
 end
